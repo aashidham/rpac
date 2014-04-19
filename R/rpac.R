d1ad6accf10059ba4e11ead8eb0b0b432aafa770
Aaditya Shidham <aashidham@gmail.com>
2014-04-19 15:39:02 -0700

    added labelled PCA

--- R/rpac.R
+++ R/rpac.R
@@ -1,2 +1,33 @@
-f <-
-function(x, y) x+y
+plotlPCA <- function(X,Y) {
+L <- matrix(-1,nrow(X),nrow(X))
+for (i in 1:nrow(L))
+{
+	for (j in 1:ncol(L))
+	{
+		if(Y[i] == Y[j])
+		{
+			L[i,j]<-0
+		}
+		if(i==j)
+		{
+			L[i,j]<-0
+		}
+	}
+}
+for (i in 1:nrow(L))
+{
+	for (j in 1:ncol(L))
+	{
+		if(i==j)
+		{
+			L[i,j]<--sum(L[i,])
+		}
+	}
+}
+A <- t(X) %*% L %*% X
+B <- t(X) %*% X
+V <- geigen(A,B)$vectors
+V <- V[,ncol(V):1]
+toplot <- X %*% V
+plot(toplot[,1],toplot[,2],col=Y)
+}
